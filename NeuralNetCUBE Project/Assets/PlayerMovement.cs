8a12c4cc45765641a7c994895bcb1e1232c965cf
Banana-Blade <jonaskeppel@googlemail.com>
2019-02-05 05:11:38 +0100

    Implemented Episode 1 to 5 of Brackeys
    Camera, Movement, Obstacle and so on

---
+++ NeuralNetCUBE Project/Assets/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    // This is a reference to the Rigidbody component called "rb"
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;  // Variable that determines the forward force
+    public float sidewaysForce = 500f;  // Variable that determines the sideways force
+
+    // We marked this as "Fixed"Update because we
+    // are using it to mess with physics.
+    void FixedUpdate()
+    {
+        // Add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d") || Input.GetKey("right"))  // If the player is pressing the "d" key
+        {
+            // Add a force to the right
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a") || Input.GetKey("left"))  // If the player is pressing the "a" key
+        {
+            // Add a force to the left
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
